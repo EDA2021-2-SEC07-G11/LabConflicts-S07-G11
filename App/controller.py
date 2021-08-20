d7c3186090db5c51b544a96c47cd966fba1ec828
MiguelSC01 <88687521+MiguelSC01@users.noreply.github.com>
2021-08-20 16:19:57 -0500

    cambios del Estudiante 2

--- App/controller.py
+++ App/controller.py
@@ -52,9 +52,7 @@ def loadTags(filename):
     return tags
 
 
-def loadBooksTags(catalog):
+def loadBooksTags(filename):
     # TODO: Modificación de Est-1 y Est-2 en el Lab 2
-    def loadBooksTags(filename):
-        # TODO: Modificación de Est-1 y Est-2 en el Lab 2
-        btfile = cf.data_dir + filename
-        return model.addBooksTags(btfile)
+    btfile = cf.data_dir + filename
+    return model.addBooksTags(btfile)
